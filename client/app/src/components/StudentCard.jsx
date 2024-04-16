588fd0ae1e4336794fb3d26cf0bd071dfe9fe2b7
brentnettles <brentnettles@gmail.com>
2024-04-16 00:53:07 -0400

    small css changes - basic site functional fullstack

--- client/app/src/components/StudentCard.jsx
+++ client/app/src/components/StudentCard.jsx
@@ -9,9 +9,18 @@ export default function StudentCard() {
     return <p>Loading...</p>;
   }
 
+  const handleCardClick = (e) => {
+    e.stopPropagation(); // Prevent click propagation to the parent overlay
+    navigate(`/student-details/${student.id}`);
+  };
+
+  const handleOverlayClick = () => {
+    navigate(`/yearbook/${student.cohort_id}`);
+  };
+
   return (
-    <div className="card-overlay" onClick={() => navigate(`/yearbook/${student.cohort_id}`)}>
-      <div className="student-card" onClick={(e) => e.stopPropagation()}>
+    <div className="card-overlay" onClick={handleOverlayClick}>
+      <div className="student-card" onClick={handleCardClick}>
         <img src={`http://127.0.0.1:5555${student.img}`} alt={student.name} />
         <div>
           <h2>{student.name}</h2>
